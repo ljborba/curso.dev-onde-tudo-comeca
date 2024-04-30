cdc29c6a721dd96886b7f27365fef5af4b94e7ea
Lucas Borba <janinglucas123@gmail.com>
2024-04-30 00:14:37 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 function Home() {
     return (
-    <p>Em comemoração ao seu aniversário, nossa reserva está marcada para às <strong>19h da noite de amanhã 29/04.</strong></p>,
-    <h5>O lugar? Surpresa!</h5>
+        <div>
+        <p>Em comemoração ao seu aniversário, nossa reserva está marcada para às <strong>19h da noite de amanhã 29/04.</strong></p>
+        <h5>O lugar? Surpresa!</h5>
+        </div>
     )
 }
 
