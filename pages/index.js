810634903f2cf080d723b1c6b333df4461b47a88
Lucas Borba <janinglucas123@gmail.com>
2024-04-30 00:12:36 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Oi Sabrina, Nossa reserva para amanhã 29/04 às 19h da noite está marcada.</h1>
+    return (
+    <p>Em comemoração ao seu aniversário, nossa reserva está marcada para às <strong>19h da noite de amanhã 29/04.</strong></p>,
+    <h5>O lugar? Surpresa!</h5>
+    )
 }
 
 export default Home;
