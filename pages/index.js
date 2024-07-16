04cc71c49773af163127af296e9bd12a212bbee5
Lucas Borba <janinglucas123@gmail.com>
2024-07-16 20:46:37 +0000

    modifica o arquivo `index.js`.

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,24 @@ function Home() {
         <div>
             <p>Em breve,<strong>Construção</strong></p>
             <p>Daqui pra baixo é só pra frente.</p>
+            <section>
+                <h6>Os assuntos que eu já passei:</h6>
+                <ul>
+                    <li>Git e GitHub</li>
+                    <li>Repositório</li>
+                    <li>Ambiente de Desenvolvimento</li>
+                    <li>Codespaces</li>
+                    <li>Protocolos</li>
+                    <li>Client e Server</li>
+                    <li>Hospedagem e Deploy</li>
+                    <li>Milestone e Issues</li>
+                    <li>Estilização de Código</li>
+                    <li>EditorConfig</li>
+                    <li>Prettier</li>
+                    <li>DNS</li>
+                    <li>Domínio</li>
+                </ul>
+            </section>
         </div>
     )
 }
