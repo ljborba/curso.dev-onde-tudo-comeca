307e0003effcbc4cbbae45d3b7a1e9adbd0118e2
Lucas Borba <janinglucas123@gmail.com>
2025-05-14 00:52:13 +0000

    Modifica arquivos `get.test.js` e `post.test.js`.

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,5 +1,11 @@
 import database from "infra/database.js";
 
+beforeAll(cleanDataBase);
+
+async function cleanDataBase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
 test("POST to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
@@ -7,7 +13,6 @@ test("POST to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  console.log(responseBody);
 
   expect(Array.isArray(responseBody)).toBe(true);
 
