afd3944495b289018a8a2593946d337bb5bc343b
Lucas Borba <janinglucas123@gmail.com>
2026-04-12 23:55:27 -0300

    feat: create and use `InternalServerError` in `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,8 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors.js";
 
 async function status(request, response) {
+  try {
   const updatedAt = new Date().toISOString();
 
   const databaseVersionResult = await database.query("SHOW server_version;");
@@ -31,6 +33,16 @@ async function status(request, response) {
       },
     },
   });
+} catch (error) {
+  const publicErrorObject = new InternalServerError({
+    cause: error
+  });
+  
+  console.log('\n Erro dentro do catch do controller:')
+  console.error(publicErrorObject);
+
+  response.status(500).json(publicErrorObject);
+}
 }
 
 export default status;
